b34c1d8eeee22977821d8cb3d72edcd5013307c7
isabelfuentes7 <84352377+isabelfuentes7@users.noreply.github.com>
2021-08-16 10:47:54 -0400

    updating

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-let map;
+var repoList = document.querySelector('ul');
+var fetchButton = document.getElementById('fetch-button');
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+function getAPi(){
+  var requestUrl ='https://cors-anywhere.herokuapp.com/https://api.covidtracking.com/v1/states/ca/current.json'; 
+
+  fetch(requestUrl)
+  .then(function (response){
+    return response.json();
+  })
+  .then(function (data){
+
+  
+
+    //console.log(data)
+    //document.getElementById("state").innerText = data.state;
+    // for (var i =0; 1 < data.length: i++) {
+    //   var listItem.textContent = data[i] .html_url;
+    //   repoList.appendChild(listItem);
+    // }
+  })
 }
+
+'https://www.vaccinespotter.org/api/v0/states/MA.json'
+
+// let map;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
+getAPi()
+
+fetchButton.addEventListener('click', getAPi);
